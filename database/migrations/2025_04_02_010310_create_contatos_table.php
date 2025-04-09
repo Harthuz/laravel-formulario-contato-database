50711f49e7503d029d46da09d09ac093b3571111
Hernandes <hernandeshass910@gmail.com>
2025-04-09 20:52:48 -0300

    feat: propriedade nullable colunas tabela contatos

--- database/migrations/2025_04_02_010310_create_contatos_table.php
+++ database/migrations/2025_04_02_010310_create_contatos_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('contatos', function (Blueprint $table) {
             $table->id();
-            $table->string('nome');
-            $table->string('telefone');
-            $table->string('origem');
-            $table->text('observacoes');
+            $table->string('nome')->nullable();
+            $table->string('telefone')->nullable();
+            $table->string('origem')->nullable();
+            $table->string('observacoes')->nullable();
             $table->timestamps();
         });
     }
